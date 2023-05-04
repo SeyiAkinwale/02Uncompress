ffcb59f5e0b69e48615672396b5937274fe85583
SeyiAkinwale <72584469+SeyiAkinwale@users.noreply.github.com>
2023-05-04 20:52:15 +0000

    correct numbers name

--- main.cpp
+++ main.cpp
@@ -3,10 +3,20 @@ using namespace std;
 
 void uncompress(string s)
 {
-  string number = "0123456789";
+  string numbers = "0123456789";
 
-  int i=0;
-  int j=0; //go until end of num seq
+  int i=0; //start of num seq
+  int j=0; //end of num seq, gives letter
+  while (j<s.length())
+  {
+    //check if char @ j is null position
+    if (numbers.find(s[j]) != string::npos)
+    {
+      j++;
+    }else{
+      stoi(s.substr(i,j));
+    }
+  }
   return s;
 }
 
